edd8f7de2b791316b5d7bfe04eaa6975a713518f
Ubuntu <ubuntu@ip-172-31-22-145.ap-northeast-1.compute.internal>
2020-10-20 08:01:10 +0000

    Finish ch03

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  
   get 'static_pages/home'
+  
   get 'static_pages/help'
-  root 'application#hello'
+  
+  get 'static_pages/about'
 end
