83aaa25885fe9849165d693d22edc6d39729b328
Ubuntu <ubuntu@ip-172-31-22-145.ap-northeast-1.compute.internal>
2020-10-22 17:24:24 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,43 @@
 class UsersController < ApplicationController
+  #GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  #GET /users/new
   def new
+    @user = User.new
+  end
+  
+  # POSt /users (+ params)
+  def create
+      #(@user + given params).save
+      # User.create(params[:user])
+      # User.create(params[:user])
+     @user =  User.new(user_params) 
+     if @user.save
+       #Success(valid params)
+       #GET "/users/#{@user.id}"
+       flash[:success] = "Welcome to the Sample App!"
+       redirect_to @user
+       #redirect_to user_path(@user)
+       #redirect_to user_path(@user.id)
+       #redirect_to user_path(1)
+       #            => /users/1
+       
+     else
+       #faliure(not validv params)
+       render 'new'
+     end  
+   end
+  
+  
+  
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
