bd7830b669c7f8b04b067cf261a227d9a105092e
Ubuntu <ubuntu@ip-172-31-22-145.ap-northeast-1.compute.internal>
2020-10-27 05:58:28 +0000

    envrionments変更2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # クラウドIDEの場合は以下をお使いください
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'yume-railstutorial.herokuapp.com'
+  host = 'yumeko-railstutorial.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
