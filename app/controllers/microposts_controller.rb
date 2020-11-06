5f27bbb5aa63b29470d17c416e7e2658b8fffac6
Ubuntu <ubuntu@ip-172-31-22-145.ap-northeast-1.compute.internal>
2020-11-06 18:48:38 +0000

    Finish13.4

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,6 +5,7 @@ class MicropostsController < ApplicationController
   #POST /microposts
   def create
     @micropost = current_user.microposts.build(micropost_params)
+    @micropost.image.attach(params[:micropost][:image])
     if @micropost.save
       flash[:success] = "Micropost created!"
       redirect_to root_url # => static_pages#homeが呼ばれている
@@ -24,7 +25,7 @@ class MicropostsController < ApplicationController
   private
 
     def micropost_params
-      params.require(:micropost).permit(:content)
+      params.require(:micropost).permit(:content, :image)
     end 
     
    def correct_user
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,8 @@ Rails.application.configure do
     :enable_starttls_auto => true
   }
   
+  # アップロードされたファイルをAWSに保存する
+  config.active_storage.service = :amazon
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
